5dbc760d98e7db22acfbda8b50557632136a5c20
Ian Lin <ian.lin123@gmail.com>
2016-10-31 17:41:55 -0400

    Data structure for reading in the lidar scans

--- scanData.py
+++ scanData.py
@@ -1,5 +1,20 @@
 ## Samuel Rohrer (rohrer) and Ian Lin
 #  October 30, 2016
 #  read in LIDAR data and convert to nice struct
+from collections import namedtuple
 
+# kinda like a c++ struct, just a simple tuple
+Scan = namedtuple("Scan", "x y r z")
 
+class Scans():
+    def __init__(self):
+        # array of 4 arrays of Scan objects
+        self.laser = [[], [], [], []]
+
+    def readLidarData(self, filename):
+        None
+
+# example: the z value for the 10th scan from laser 2 is
+scans = Scans()
+# index from 0
+scans[1][9].z
